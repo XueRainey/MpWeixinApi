b7b6c7fbd317decc8e44c9b41022e78f8efa5e67
Rainey <515385036@qq.com>
2017-11-14 13:45:42 +0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const fs = require('fs')
 const path = require('path')
-const { MpWeixin } = require('./lib')
+const { WeiXin } = require('./lib')
 
 ;(async function run () {
   const userInfoCachePath = path.resolve(__dirname, './cache/userInfo.json')
-  const platformer = new MpWeixin(require(userInfoCachePath), {
+  const platformer = new WeiXin(require(userInfoCachePath), {
     cachePath: path.resolve(__dirname, './cache')
   })
   platformer.addEventListener('onUserInfoChange', () => {
     fs.writeFileSync(userInfoCachePath, JSON.stringify(platformer.getUserInfo(), null, 4))
   })
   if (!await platformer.checkLogin()) await platformer.login()
+  const platformerInfo = await platformer.getPlatformerInfo(platformer)
+  console.log('platformerInfo', platformerInfo)
 })()
