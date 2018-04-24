857f4b3d18bcf808570c3e07cc0f5f2adc4556cf
Rainey <515385036@qq.com>
2018-04-24 14:15:58 +0800

    update

--- test/weixin.js
+++ test/weixin.js
@@ -5,12 +5,15 @@ const { WeiXin } = require('../lib')
 ;(async function run () {
   const userInfoCachePath = path.resolve(__dirname, '../cache/weixin.userinfo.json')
   const platformer = new WeiXin(require(userInfoCachePath), {
-    cachePath: path.resolve(__dirname, '../cache'),
-    debug: true
+    debug: false
   })
 
-  platformer.addListener('loginHook', (status) => {
+  platformer.addListener('loginHook', (status, data) => {
     console.log(status)
+    if (status === 'qrImageDownload') {
+      // `data:${data.fileType};base64,${data.buffer.toString('base64')}`
+      fs.writeFileSync('./ab.jpeg', data.buffer.toString('binary'), 'binary', console.log)
+    }
     if (status === 'finish') {
       fs.writeFileSync(userInfoCachePath, JSON.stringify(platformer.info(), null, 4))
     }
